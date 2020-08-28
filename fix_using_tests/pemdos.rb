52c2113895b750beabc424e45347f7ecac72e37d
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-28 15:24:25 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,6 @@
-# don't forget to add: require 'pry'
+require "pry"
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
-  end
+  10.times {print "s"}
+  print "surprise!"
 end
